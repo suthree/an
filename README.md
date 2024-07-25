7ba22657e63e9c40d92ce639993b147d7c07582b
alexsuthree <alexsuthree@gmail.com>
2024-07-25 15:24:50 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # an
+
 Powerful Stock System By ChangAn
+
+# ceres
+
+- 丰收女神
+
+# 盘中餐
+
+- pan 盘
+
+  - 数据
+
+- zhong 中
+
+  - 通知
+
+- can 餐
+  - 代码
---
+++ ceres/pan/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PanConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pan'
